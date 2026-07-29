df5b571b3f029076a5d490882a29d1f6193d49df
noriaki watanabe <nabeyang@gmail.com>
2026-07-30 08:54:11 +0900

    test: verify complete OAuth scope list

--- Tests/SwiftTangledTests/OAuth/TangledClientInfoTests.swift
+++ Tests/SwiftTangledTests/OAuth/TangledClientInfoTests.swift
@@ -9,10 +9,54 @@ import Testing
     info.clientId
       == "https://soyokaze-pds-rc-677008170211.asia-northeast1.run.app/tangled/cli-client-metadata.json"
   )
-  #expect(info.scopes.first == "atproto")
-  #expect(info.scopes.contains("repo:sh.tangled.feed.star"))
-  #expect(info.scopes.contains("rpc:sh.tangled.repo.create?aud=*"))
-  #expect(info.scopes.contains("identity:handle"))
+  #expect(
+    info.scopes == [
+      "atproto",
+      "repo:sh.tangled.actor.profile",
+      "repo:sh.tangled.feed.comment",
+      "repo:sh.tangled.feed.reaction",
+      "repo:sh.tangled.feed.star",
+      "repo:sh.tangled.graph.follow",
+      "repo:sh.tangled.graph.vouch",
+      "repo:sh.tangled.knot",
+      "repo:sh.tangled.knot.member",
+      "repo:sh.tangled.label.definition",
+      "repo:sh.tangled.label.op",
+      "repo:sh.tangled.publicKey",
+      "repo:sh.tangled.repo",
+      "repo:sh.tangled.repo.artifact",
+      "repo:sh.tangled.repo.collaborator",
+      "repo:sh.tangled.repo.issue",
+      "repo:sh.tangled.repo.issue.comment",
+      "repo:sh.tangled.repo.issue.state",
+      "repo:sh.tangled.repo.pull",
+      "repo:sh.tangled.repo.pull.comment",
+      "repo:sh.tangled.repo.pull.status",
+      "repo:sh.tangled.spindle",
+      "repo:sh.tangled.spindle.member",
+      "repo:sh.tangled.string",
+      "blob:*/*",
+      "rpc:sh.tangled.knot.addMember?aud=*",
+      "rpc:sh.tangled.knot.removeMember?aud=*",
+      "rpc:sh.tangled.ci.triggerPipeline?aud=*",
+      "rpc:sh.tangled.ci.cancelPipeline?aud=*",
+      "rpc:sh.tangled.repo.addCollaborator?aud=*",
+      "rpc:sh.tangled.repo.addSecret?aud=*",
+      "rpc:sh.tangled.repo.create?aud=*",
+      "rpc:sh.tangled.repo.delete?aud=*",
+      "rpc:sh.tangled.repo.deleteBranch?aud=*",
+      "rpc:sh.tangled.repo.forkStatus?aud=*",
+      "rpc:sh.tangled.repo.forkSync?aud=*",
+      "rpc:sh.tangled.repo.hiddenRef?aud=*",
+      "rpc:sh.tangled.repo.listSecrets?aud=*",
+      "rpc:sh.tangled.repo.merge?aud=*",
+      "rpc:sh.tangled.repo.mergeCheck?aud=*",
+      "rpc:sh.tangled.repo.removeCollaborator?aud=*",
+      "rpc:sh.tangled.repo.removeSecret?aud=*",
+      "rpc:sh.tangled.repo.setDefaultBranch?aud=*",
+      "identity:handle",
+    ]
+  )
   #expect(info.scopes.count == Set(info.scopes).count)
   #expect(!info.scopes.contains("transition:generic"))
   #expect(!info.scopes.contains("repo:*"))
