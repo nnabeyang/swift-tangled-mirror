cb9b3744a51b3419c534e090e9bca658d2859409
noriaki watanabe <nabeyang@gmail.com>
2026-07-30 15:25:42 +0900

    chore: bump version to 0.4.0

--- Sources/SwiftTangled/SwiftTangled.swift
+++ Sources/SwiftTangled/SwiftTangled.swift
@@ -1,3 +1,3 @@
 public enum SwiftTangled {
-  public static let version = "0.3.0"
+  public static let version = "0.4.0"
 }
--- Tests/SwiftTangledTests/TangledErrorTests.swift
+++ Tests/SwiftTangledTests/TangledErrorTests.swift
@@ -4,7 +4,7 @@ import Testing
 @testable import SwiftTangled
 
 @Test func versionIsExposed() {
-  #expect(SwiftTangled.version == "0.3.0")
+  #expect(SwiftTangled.version == "0.4.0")
 }
 
 @Test func tangledErrorCasesArePatternMatchable() {
