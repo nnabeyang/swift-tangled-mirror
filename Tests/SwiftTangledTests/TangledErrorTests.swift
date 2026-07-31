cbeca426af3cd04bf0703be466a177b9966b1773
noriaki watanabe <nabeyang@gmail.com>
2026-07-31 12:44:47 +0900

    chore: bump version to 0.4.1

--- Tests/SwiftTangledTests/TangledErrorTests.swift
+++ Tests/SwiftTangledTests/TangledErrorTests.swift
@@ -4,7 +4,7 @@ import Testing
 @testable import SwiftTangled
 
 @Test func versionIsExposed() {
-  #expect(SwiftTangled.version == "0.4.0")
+  #expect(SwiftTangled.version == "0.4.1")
 }
 
 @Test func tangledErrorCasesArePatternMatchable() {
